e94be8d33cdeff04b76f5d26e302447eca744b40
Rwan <rwankhalidmaroof@gmail.com>
2025-11-19 22:34:03 +0300

    v1

---
+++ src/Array/Arayss.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Arayss {
+
+    public void R_Travler(int[]a) {
+        System.out.println("the element are: ");
+        for (int i=a.length-1;i>=0;i--)
+            System.out.println("Tndex["+i+"]= "+a[i]);
+    }
+
+}
---
+++ src/Array/Main.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int[]numb={1,2,3,4,5};
+       // int[]numd2=new int[5];
+       // System.out.println(Arrays.toString(numb));
+      // for (int i=0;i<numb.length;i++){
+
+         //  System.out.print(numb[i]+" ");
+
+        Arayss A=new Arayss();
+        A.R_Travler(numb);
+
+       }
+
+
+
+    }
+
+
+
